e7317a3a0d3857d63f6695e4f759940e6a2c6a30
PinxuanHuang <hatedog77777@gmail.com>
2026-04-27 02:46:02 +0800

    [Update] Fix Makefile and Add file_operations initialize functions
    Description:
    	- Fix Makefile syntax error
    	- Add file_operations initialize and functions implementation

--- 002_pseudo_char_driver/main.c
+++ 002_pseudo_char_driver/main.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/kdev_t.h>
 
 #define DEV_MEM_SIZE 512
 
@@ -10,16 +12,58 @@ char device_buffer[DEV_MEM_SIZE];
 /* Cdev variable */
 struct cdev pcd_cdev;
 
+loff_t pcd_lseek(struct file *filp, loff_t off, int whence)
+{
+    pr_info("lseek requested\n");
+    return 0;
+}
+
+ssize_t pcd_read(struct file *filp, char __user *buff, size_t count, loff_t *f_pos)
+{
+    pr_info("read requested for %zu bytes\n", count);
+    return 0;
+}
+
+ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
+{
+    pr_info("write requested for %zu bytes\n", count);
+    return 0;
+}
+
+int pcd_open(struct inode *inode, struct file *filp)
+{
+    pr_info("open requested\n");
+    return 0;
+}
+
+int pcd_release(struct inode *inode, struct file *filp)
+{
+    pr_info("release requested\n");
+    return 0;
+}
+
 /* file operations of the driver */
-struct file_operations pcd_fops;
+struct file_operations pcd_fops = {
+    .open = pcd_open,
+    .write = pcd_write,
+    .read = pcd_read,
+    .llseek = pcd_lseek,
+    .release = pcd_release,
+    .owner = THIS_MODULE};
 
 /* hold device number */
 dev_t device_number;
 
+struct class *class_pcd;
+
+struct device *device_pcd;
+
 static int __init pcd_driver_init(void)
 {
     /* Dynamically allocate a device number */
-    alloc_chrdev_region(&device_number, 0, 1, "pcd");
+    alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+
+    pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
 
     /* Initialize the cdev structure with fops */
     cdev_init(&pcd_cdev, &pcd_fops);
@@ -28,12 +72,30 @@ static int __init pcd_driver_init(void)
     pcd_cdev.owner = THIS_MODULE;
     cdev_add(&pcd_cdev, device_number, 1);
 
+    /* create device class under /sys/class/ */
+    class_pcd = class_create(THIS_MODULE, "pcd_class");
+
+    /* populate the sysfs with device information */
+    device_pcd = device_create(&class_pcd, NULL, device_number, NULL, "pcd");
+
+    pr_info("PCD module init successful\n");
+
     return 0;
 }
 
 static void __exit pcd_driver_cleanup(void)
 {
     return 0;
+}
+
+static void pcd_driver_cleanup(void)
+{
+    device_destroy(class_pcd, device_number);
+    class_destroy(class_pcd);
+    cdev_del(&pcd_cdev);
+    unregister_chrdev_region(device_number, 1);
+    pr_info("module unregister\n");
+    return;
 }
 
 module_init(pcd_driver_init);
