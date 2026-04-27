50cf32d8fa4f782ac2077047324b7a503de5dcea
PinxuanHuang <hatedog77777@gmail.com>
2026-04-28 03:40:49 +0800

    [Update] Add error handling for driver init function

--- 002_pseudo_char_driver/main.c
+++ 002_pseudo_char_driver/main.c
@@ -129,8 +129,15 @@ struct device *device_pcd;
 
 static int __init pcd_driver_init(void)
 {
+    int ret;
+
     /* Dynamically allocate a device number */
-    alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+    ret = alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+    if (ret < 0)
+    {
+        pr_err("Alloc chrdev failed\n");
+        goto out;
+    }
 
     pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
 
@@ -139,17 +146,43 @@ static int __init pcd_driver_init(void)
 
     /* register a device cdev structure with VFS */
     pcd_cdev.owner = THIS_MODULE;
-    cdev_add(&pcd_cdev, device_number, 1);
+    ret = cdev_add(&pcd_cdev, device_number, 1);
+    if (ret < 0)
+    {
+        pr_err("cdev_add failed\n");
+        goto unreg_chrdev;
+    }
 
     /* create device class under /sys/class/ */
     class_pcd = class_create(THIS_MODULE, "pcd_class");
+    if (IS_ERR(class_pcd))
+    {
+        pr_err("Class creation failed\n");
+        ret = PTR_ERR(class_pcd);
+        goto cdev_del;
+    }
 
     /* populate the sysfs with device information */
     device_pcd = device_create(&class_pcd, NULL, device_number, NULL, "pcd");
+    if (IS_ERR(device_pcd))
+    {
+        pr_err("device creation failed\n");
+        ret = PTR_ERR(device_pcd);
+        goto class_del;
+    }
 
     pr_info("PCD module init successful\n");
 
     return 0;
+class_del:
+    class_destroy(class_pcd);
+cdev_del:
+    cdev_del(&pcd_cdev);
+unreg_chrdev:
+    unregister_chrdev_region(device_number, 1);
+out:
+    pr_info("Module insertion failed\n");
+    return ret;
 }
 
 static void __exit pcd_driver_cleanup(void)
