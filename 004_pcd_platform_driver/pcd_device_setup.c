7d0c2215fe6d0da59963478ca3b5e034490e219a
PinxuanHuang <hatedog77777@gmail.com>
2026-05-12 00:50:39 +0800

    [Update] Update pcd_device_setup
    Description:
    	- replace pdev register API to platform_add_devices
    	- Update pcd dev data

--- 004_pcd_platform_driver/pcd_device_setup.c
+++ 004_pcd_platform_driver/pcd_device_setup.c
@@ -7,44 +7,72 @@ void pcdev_release(struct device *dev)
     pr_info("Device release\n");
 }
 
-struct pcdev_platform_data pcdev_pdata[2] =
-    {
-        /* data */
-        [0] = {.size = 512, .perm = RDWR, .serial_number = "PCDEVABC1111"},
-        [1] = {.size = 1024, .perm = RDWR, .serial_number = "PCDEVXYZ2222"}};
+/* create n platform data */
+struct pcdev_platform_data pcdev_pdata[] = {
+    [0] = {.size = 512, .perm = RDWR, .serial_number = "PCDEVABC1111"},
+    [1] = {.size = 1024, .perm = RDWR, .serial_number = "PCDEVXYZ2222"},
+    [2] = {.size = 128, .perm = RDONLY, .serial_number = "PCDEVXYZ3333"},
+    [3] = {.size = 32, .perm = WRONLY, .serial_number = "PCDEVXYZ4444"}};
 
-struct platform_device platform_pcdev_1 =
-    {
-        /* data */
-        .name = "pseudo-char-device",
-        .id = 0,
-        .dev = {
-            .platform_data = &pcdev_pdata[0],
-            .release = pcdev_release}};
+/* create n platform devices */
 
-struct platform_device platform_pcdev_2 =
+struct platform_device platform_pcdev_1 = {
+    .name = "pcdev-A1x",
+    .id = 0,
+    .dev = {
+        .platform_data = &pcdev_pdata[0],
+        .release = pcdev_release}};
+
+struct platform_device platform_pcdev_2 = {
+    .name = "pcdev-B1x",
+    .id = 1,
+    .dev = {
+        .platform_data = &pcdev_pdata[1],
+        .release = pcdev_release}};
+
+struct platform_device platform_pcdev_3 = {
+    .name = "pcdev-C1x",
+    .id = 2,
+    .dev = {
+        .platform_data = &pcdev_pdata[2],
+        .release = pcdev_release}};
+
+struct platform_device platform_pcdev_4 = {
+    .name = "pcdev-D1x",
+    .id = 3,
+    .dev = {
+        .platform_data = &pcdev_pdata[3],
+        .release = pcdev_release}};
+
+struct platform_device *platform_pcdevs[] =
     {
-        /* data */
-        .name = "pseudo-char_device",
-        .id = 1,
-        .dev = {
-            .platform_data = &pcdev_pdata[1],
-            .release = pcdev_release}};
+        &platform_pcdev_1,
+        &platform_pcdev_2,
+        &platform_pcdev_3,
+        &platform_pcdev_4};
 
 static int __init pcdev_platform_init(void)
 {
-    /* register platform device */
-    platform_device_register(&platform_pcdev_1);
-    platform_device_register(&platform_pcdev_2);
+    /* register n platform devices */
+
+    // platform_device_register(&platform_pcdev_1);
+    // platform_device_register(&platform_pcdev_2);
+
+    platform_add_devices(platform_pcdevs, ARRAY_SIZE(platform_pcdevs));
+
+    pr_info("Device setup module loaded \n");
 
     return 0;
 }
 
 static void __exit pcdev_platform_exit(void)
 {
+
     platform_device_unregister(&platform_pcdev_1);
     platform_device_unregister(&platform_pcdev_2);
-    pr_info("Device setup module removed\n");
+    platform_device_unregister(&platform_pcdev_3);
+    platform_device_unregister(&platform_pcdev_4);
+    pr_info("Device setup module unloaded \n");
 }
 
 module_init(pcdev_platform_init);
